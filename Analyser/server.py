f50ad1b364b082c781d3e86d2f7e794a7ea2a6d1
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-08-01 19:56:40 +0200

    add server

--- Analyser/server.py
+++ Analyser/server.py
@@ -1,24 +1,32 @@
 from PIL import Image, ImageFile
 import pprint
 import io
+import json
 
 async def app(scope, receive, send):
     assert scope['type'] == 'http'
 
-    incoming = await receive()
+    body = b''
 
-    body = incoming['body']
+    more_body = True
+    while more_body:
+        event = await receive()
+        body += event['body']
+        more_body = event['more_body']
 
-    print(scope)
-    print(incoming)
+    # print(len(binary_image))
 
-    # ImageFile.LOAD_TRUNCATED_IMAGES = True
+    # ImageFile.LOAD_TRUNCATED_IMAGES = False
 
-    # stream = io.BytesIO(body)
+    # stream = io.BytesIO(binary_image)
 
     # image = Image.open(stream)
 
     # image.show()
+
+    as_json: json = json.loads(body)
+
+    print(as_json)
 
     await send({
         'type': 'http.response.start',
