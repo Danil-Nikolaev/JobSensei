38c845a712f330f90d1d2e502b0f6a86bfd76bb7
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-09-20 22:17:58 +0300

    Refactoring code in HHAPI (change class map to JsonNode)

--- JobSensei/src/main/java/com/nikolaev/JobSensei/API/HHAPI.java
+++ JobSensei/src/main/java/com/nikolaev/JobSensei/API/HHAPI.java
@@ -1,27 +1,26 @@
 package com.nikolaev.JobSensei.API;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.nikolaev.JobSensei.converter.ConverterHH;
 
 public class HHAPI extends JobAPI {
 
     ObjectMapper mapper;
-    List<Map<String, Object>> listMap;
+    ArrayNode jsonArray;
 
     public HHAPI() {
         super(new ConverterHH());
         this.mapper = new ObjectMapper();
-        this.listMap = new ArrayList<>();
+        this.jsonArray = mapper.createArrayNode();
     }
 
     @Override
@@ -31,12 +30,12 @@ public class HHAPI extends JobAPI {
         String result = "Error";
 
         for (int page = 0; page < pages; page++) {
-            String json = getJson(profession, String.valueOf(page));
+            JsonNode json = getJson(profession, String.valueOf(page));
             getVacancies(json);
         }
 
         try {
-            result = this.mapper.writeValueAsString(this.listMap);
+            result = this.mapper.writeValueAsString(this.jsonArray);
         } catch (Exception e) {
             System.out.println("Module - HHAPI, method - getAllJson");
             System.out.println("convert from json into String");
@@ -47,73 +46,13 @@ public class HHAPI extends JobAPI {
 
     }
 
-    private void addJson(String json) {
-        try {
-            Map<String, Object> jsonMap = this.mapper.readValue(json, new TypeReference<Map<String, Object>>() {
-            });
-            this.listMap.add(jsonMap);
-        } catch (Exception e) {
-            System.out.println("Module - HHAPI, method - addJSon");
-            System.out.println(e.getClass());
+    private int getPages(String profession) {
+        String jsonString = getJson(profession);
+        JsonNode rootNode = stringToJsonNode(jsonString);
+        if (rootNode.isObject()) {
+            return rootNode.get("pages").asInt();
         }
-    }
-
-    private void getVacancies(String json) {
-        try {
-            List<Map<String, Object>> items = this.mapper.readValue(json, List.class);
-            for (Map<String, Object> item : items) {
-                String url = String.valueOf (item.get("url"));
-                try {
-                    TimeUnit.MILLISECONDS.sleep(500);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                System.out.println(url);
-                addJson(getVacancy(url));
-            }
-        } catch (Exception e) {
-            System.out.println("Module - HHAPI method - getVacancies");
-            System.out.println(e.getClass());
-        }
-    }
-
-    private String getJson(String profession, String page) {
-        String apiUrl = "https://api.hh.ru/vacancies?per_page=100&search_field=name&text=" + profession + "&page=" + page;
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            String body = response.getBody();
-            try {
-                    Map<String, Object> BodyMap = this.mapper.readValue(body, new TypeReference<Map<String, Object>>() {
-                    });
-                    String itemsString = this.mapper.writeValueAsString(BodyMap.get("items"));
-                    return itemsString;
-                } catch (Exception e) {
-                    System.out.println("Module - HHAPI, method - getJson");
-                    System.out.println("Not find items or can`t convert");
-                    System.out.println(e.getClass());
-                }
-        }
-        return null;
-    }
-
-    private String getVacancy(String url) {
-        try{
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-           
-            if (response.getStatusCode() == HttpStatus.OK) {
-                return response.getBody();
-            }
-        }catch (Exception e) {
-            System.out.println("GetVacancy");
-            System.out.println(e.getClass());
-        }
-        return null;
-      
-
+        return -1;
     }
 
     private String getJson(String profession) {
@@ -126,17 +65,72 @@ public class HHAPI extends JobAPI {
         return null;
     }
 
-    private int getPages(String profession) {
-        String json = getJson(profession);
+    private JsonNode stringToJsonNode(String json) {
         try {
-            Map<String, Object> jsonMap = this.mapper.readValue(json, new TypeReference<Map<String, Object>>() {
-            });
-            return (int) jsonMap.get("pages");
-        } catch (Exception e) {
-            System.out.println("Module - HHAPI, method - getPages");
-            System.out.println(e.getClass());
+            JsonNode root = this.mapper.readTree(json);
+             return root;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
-        return -1;
+       return null;
     }
 
+    private JsonNode getJson(String profession, String page) {
+        String apiUrl = "https://api.hh.ru/vacancies?per_page=100&search_field=name&text=" + profession + "&page=" + page;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            String body = response.getBody();
+            JsonNode rootNode = stringToJsonNode(body);
+            rootNode = rootNode.get("items");
+            return rootNode;
+        }
+        return null;
+    }
+
+    private void addJson(String json) {
+        if (json.equals(json)) return;
+
+        JsonNode jsonNode = stringToJsonNode(json);
+        this.jsonArray.add(jsonNode);
+    }
+
+    private void sleep() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void getVacancies(JsonNode jsonNode) {
+        for (JsonNode item : jsonNode) {
+            String url = item.get("url").asText();
+            sleep();
+            addJson(getVacancy(url));
+        }
+    }
+
+    private String getVacancy(String url) {
+        try{
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+           
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return response.getBody();
+            }
+
+            if (response.getStatusCode() == HttpStatus.FORBIDDEN) {
+                return "error";
+            }
+        }catch (Exception e) {
+            System.out.println("GetVacancy");
+            System.out.println(e.getClass());
+        }
+        return null;
+      
+
+    }
 }
